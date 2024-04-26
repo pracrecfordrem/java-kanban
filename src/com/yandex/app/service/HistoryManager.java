69e1365f700a20c3e6e5272757165918bfcb1bd5
recfordrem <rbarinov@diasoft.ru>
2024-04-26 16:25:39 +0300

    5 iteration

--- src/com/yandex/app/service/HistoryManager.java
+++ src/com/yandex/app/service/HistoryManager.java
@@ -2,11 +2,12 @@ package com.yandex.app.service;
 
 import com.yandex.app.model.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface HistoryManager {
     void add(Task task);
+
     void remove(int id);
+
     List<Task> getHistory();
 }
